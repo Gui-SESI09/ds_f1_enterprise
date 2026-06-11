4887d3acdc1d74f367dca705d1089e334e22e2ee
GUILHERME DALSASSO MACHADO <guilherme_machado18@estudante.sesisenai.org.br>
2026-06-11 14:07:03 -0300

    refactor: organizando rotas em arquivos separados

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { Pool } from "pg";
+import rotasUsuario from "./src/routes/usuarios.js";
 
 const sql = new Pool ({
     host: 'localhost',
@@ -11,37 +12,8 @@ const sql = new Pool ({
 
 const servidor = Fastify();
 
-servidor.get('/usuario', () => {
-    return sql.query('select * from usuarios')
-})
-
-servidor.post('/usuario', async (request, reply) => {
-    const body = request.body;
-     if (!body || !body.nome || !body.email || !body.senha_hash){
-        reply.status(400).send({error:"Preencha todas as informações corretamente!"})
-    }
-    const resultado = await sql.query(`INSERT INTO usuarios (nome, email, senha_hash) VALUES ($1, $2, $3)`, [body.nome, body.email, body.senha_hash]);
-    reply.status(200).send({message: "Tudo foi inserido corretamente."})
-})
-    
-
-servidor.put('/usuario/:id', async (request, reply) => {
-    const id = request.params.id;
-    const body = request.body;
-    const resultado = await sql.query(`UPDATE usuarios SET nome = $1, email = $2, senha_hash = $3 WHERE id = $4 RETURNING nome`, [body.nome, body.email, body.senha_hash, id]) 
-    const nomeAtualizado = resultado.rows[0].nome
-    reply.status(200).send({message: "Os dados do usuario: " + nomeAtualizado + " foram atualizados!"})
-    
-    if (resultado.rows.length === 0) {
-        reply.status(404).send({message: "O ID inserido não foi encontrado no sistema."})
-    }
-})
-
-/*
-servidor.delete()
-
-*/
+servidor.register(rotasUsuario, { sql });
 
 servidor.listen({
     port: 3000
-})
+});
---
+++ src/routes/usuarios.js
@@ -0,0 +1,30 @@
+export default async function rotasUsuario(servidor, options) {
+
+    const {sql} = options;
+    servidor.get('/usuario', async () => {
+    const resultado = await sql.query('select * from usuarios');
+    return resultado.rows; 
+});
+
+    servidor.post('/usuario', async (request, reply) => {
+    const body = request.body;
+     if (!body || !body.nome || !body.email || !body.senha_hash){
+        reply.status(400).send({error:"Preencha todas as informações corretamente!"})
+    }
+    const resultado = await sql.query(`INSERT INTO usuarios (nome, email, senha_hash) VALUES ($1, $2, $3)`, [body.nome, body.email, body.senha_hash]);
+    reply.status(200).send({message: "Tudo foi inserido corretamente."})
+});
+
+servidor.put('/usuario/:id', async (request, reply) => {
+    const id = request.params.id;
+    const body = request.body;
+    const resultado = await sql.query(`UPDATE usuarios SET nome = $1, email = $2, senha_hash = $3 WHERE id = $4 RETURNING nome`, [body.nome, body.email, body.senha_hash, id]) 
+    const nomeAtualizado = resultado.rows[0].nome
+    reply.status(200).send({message: "Os dados do usuario: " + nomeAtualizado + " foram atualizados!"})
+    
+    if (resultado.rows.length === 0) {
+        reply.status(404).send({message: "O ID inserido não foi encontrado no sistema."})
+    }
+})
+
+}
